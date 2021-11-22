956d7e95198e4d848135446a060d7a19e02919ba
kyyeo <yeokaiyu@gmail.com>
2021-11-22 23:31:18 +0800

    files

--- script.js
+++ script.js
@@ -1,9 +1,50 @@
-let myLibrary = [];
+let myLibrary = [
+  fiction = {
+    title: "charlie and the chocolate factory",
+    author: "roald dahl"
+  },
+  nonFiction = {
+    title: "blink",
+    author: "malcolm gladwell"
+  }
+];
 
-function Book() {
+function Book(title, author) {
   // the constructor...
+  this.title = title,
+  this.author = author
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(book) {
   // do stuff here
+  myLibrary.push(book);
+}
+
+const shelf = document.querySelector('.shelf')
+
+function showBook(book){
+  const div = document.createElement('div');
+  div.classList.add('book');
+  div.style.height = 300;
+  div.style.width = 300;
+
+  const bookTitle = document.createElement('p');
+  const bookAuthor = document.createElement('p');
+  bookTitle.textContent = book.title
+  bookAuthor.textContent = book.author
+
+  div.appendChild(bookTitle)
+  div.appendChild(bookAuthor)
+
+  shelf.appendChild(div);
+}
+
+function displayLibrary(library) {
+  for(let i = 0; i<library.length; i++){
+    console.log(library[i])
+    // console.log(library[i].author)
+    // console.log(library[i].title)
+
+    showBook(library[i])
+  }
 }
